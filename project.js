5b56f4b63c224d6c99e01c0a50b640e6b341f569
Jordi Viles <jvilesheras@hotmail.com>
2017-08-29 12:37:17 +0200

    start to do the board

--- project.js
+++ project.js
@@ -2,10 +2,18 @@
 function Board (rows,columns){
 	this.rows    = rows;
 	this.columns = columns;
+
+	this.generateGrid();
 }
 
 Board.prototype.generateGrid = function(){
 	console.log("generate grid running");
+	$("#board").append("<section>hjlk</section>");
+	for(x=0,x<row,x++){
+		for(y=0, y<columns,y++){
+			$("#board").append("<div>cell</div>");
+		}
+	}
 }
 
 
