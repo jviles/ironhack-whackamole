3f7c77e41eb2f4ab6de2382323d43780064c57ab
Jordi Viles <jvilesheras@hotmail.com>
2017-08-29 12:19:02 +0200

    Constructor created & prototype created

--- project.js
+++ project.js
@@ -1,5 +1,30 @@
+
+function Board (rows,columns){
+	this.rows    = rows;
+	this.columns = columns;
+}
+
+Board.prototype.generateGrid = function(){
+	console.log("generate grid running");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var board;
+
 $(document).ready(function() {
  
   console.log("jquery running");
- 
+  board = new Board(4,4);
 });
